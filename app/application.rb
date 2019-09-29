af7e72410b877c4b27e1a20cef2cb02c8eb6feee
Austin Asbury <acasbury25@aol.com>
2019-09-29 22:56:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-
+      search_item = req.params["q"]
+      if Items.all.include?(search_item)
+        resp.write "#{Items.all {|item| item.price if item.name = search_item}}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 404
