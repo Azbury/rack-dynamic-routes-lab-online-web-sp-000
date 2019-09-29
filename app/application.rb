c52f90c4cac2fec73fe8b686bc69a0b4350c8221
Austin Asbury <acasbury25@aol.com>
2019-09-29 23:06:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       search_item = req.path.split("/items/").last
-      if Item.all.include?(search_item)
+      if Item.all {|item| item.name == search_item}
         resp.write "#{Item.all {|item| item.price if item.name == search_item}}"
       else
         resp.write "Item not found"
